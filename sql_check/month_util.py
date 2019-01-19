8725e27dab6db7385761539a2125397fef41e7b6
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-01-19 10:42:43 +0100

    move sources to sub directory and define month_util.previous_month()

--- sql_check/month_util.py
+++ sql_check/month_util.py
@@ -1,9 +1,20 @@
+from datetime import date
+
+JANUARY = 1
+DECEMBER = 12
+
+
 def previous_month():
     """
     get the first day of the previous month
     :return: date object set at 1st day of previous month
     """
-    return None
+    today = date.today()
+
+    result_month = today.month - 1 if today.month is not JANUARY else DECEMBER
+    result_year = today.year if today.month is not JANUARY else today.year - 1
+
+    return today.replace(year=result_year, month=result_month, day=1)
 
 
 def month_from_str(year_month):
--- tests/test_month_util.py
+++ tests/test_month_util.py
@@ -2,16 +2,16 @@ from datetime import date
 
 import mock
 
-import month_util
+from sql_check import month_util
 
 
-@mock.patch("month_util.date")
+@mock.patch("sql_check.month_util.date")
 def test_previous_month_nominal_case(mock_date):
     mock_date.today.return_value = date(2016, 6, 24)
     assert month_util.previous_month() == date(2016, 5, 1)
 
 
-@mock.patch("month_util.date")
+@mock.patch("sql_check.month_util.date")
 def test_previous_month_january_case(mock_date):
     mock_date.today.return_value = date(2017, 1, 1)
     assert month_util.previous_month() == date(2016, 12, 1)
