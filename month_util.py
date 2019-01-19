ee7b972559ac36f0fa4951f86944dbd80aff7eb1
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-01-19 09:48:17 +0100

    initial commit

---
+++ month_util.py
@@ -0,0 +1,15 @@
+def previous_month():
+    """
+    get the first day of the previous month
+    :return: date object set at 1st day of previous month
+    """
+    return None
+
+
+def month_from_str(year_month):
+    """
+    get the first day of the month represented by year_month
+    :param year_month: str which four first characters are the year and last two the month; "201901" is Jan 2019
+    :return: date object set the the 1st day of specified month
+    """
+    return None
---
+++ tests/test_month_util.py
@@ -0,0 +1,6 @@
+def test_previous_month():
+    assert False
+
+
+def test_month_from_str():
+    assert False
