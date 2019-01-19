54877c804b01082386df8873bfe652378417adaa
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-01-19 17:48:43 +0100

    add function to convert file to single line string

---
+++ sql_check/query_runner.py
@@ -0,0 +1,9 @@
+def str_from_file(file_path):
+    """
+    Return content of file as a single line, removing indentation and SQL comments
+
+    :param file_path: whose content will be extracted as a single line
+    :type file_path: str
+    :return: the extracted string
+    """
+    return None
---
+++ tests/test_query_runner.py
@@ -0,0 +1,2 @@
+def str_from_file():
+    assert False
