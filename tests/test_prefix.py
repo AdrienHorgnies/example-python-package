da97c8fda759eabb940ef3c2f8ec72a15ed95cd0
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-01-19 16:33:41 +0100

    write tests implementation

--- tests/test_prefix.py
+++ tests/test_prefix.py
@@ -1,2 +1,42 @@
-def test_timestamp():
-    assert False
+import filecmp
+import os
+import tempfile
+from datetime import datetime
+
+import mock
+
+import prefix
+
+
+@mock.patch("prefix.datetime")
+def test_timestamp_no_dir(mock_datetime):
+    mock_datetime.now.return_value = datetime(2019, 4, 2, 10, 20, 33)
+
+    path = os.path.join(tempfile.gettempdir(), "tmp-test-file.sql")
+    with open(path, "w") as file:
+        file.write("SELECT 'This is a test';")
+
+    copy_path = prefix.timestamp(path)
+
+    assert os.path.isfile(copy_path)
+    assert os.path.dirname(copy_path) == os.path.dirname(path)
+    assert os.path.basename(copy_path) == "2019-04-02T10-20-33_tmp-test-file.sql"
+    assert filecmp.cmp(copy_path, path)
+
+
+@mock.patch("prefix.datetime")
+def test_timestamp_with_dir(mock_datetime):
+    mock_datetime.now.return_value = datetime(2019, 4, 2, 10, 20, 33)
+
+    directory = tempfile.gettempdir()
+    path = os.path.join(tempfile.gettempdir(), "tmp-test-file.sql")
+
+    with open(path, "w") as file:
+        file.write("SELECT 'This is a test';")
+
+    copy_path = prefix.timestamp(path, directory=directory)
+
+    assert os.path.isfile(copy_path)
+    assert os.path.dirname(copy_path) == directory
+    assert os.path.basename(copy_path) == "2019-04-02T10-20-33_tmp-test-file.sql"
+    assert filecmp.cmp(copy_path, path)
