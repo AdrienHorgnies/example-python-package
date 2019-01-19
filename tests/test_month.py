127d6b7b08a03c5366bc4b16900b85ed94b6f1b5
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-01-19 16:59:10 +0100

    import module without specifying package

--- tests/test_month.py
+++ tests/test_month.py
@@ -2,16 +2,16 @@ from datetime import date
 
 import mock
 
-from sql_check import month
+import month
 
 
-@mock.patch("sql_check.month.date")
+@mock.patch("month.date")
 def test_previous_nominal_case(mock_date):
     mock_date.today.return_value = date(2016, 6, 24)
     assert month.previous() == date(2016, 5, 1)
 
 
-@mock.patch("sql_check.month.date")
+@mock.patch("month.date")
 def test_previous_january_case(mock_date):
     mock_date.today.return_value = date(2017, 1, 1)
     assert month.previous() == date(2016, 12, 1)
