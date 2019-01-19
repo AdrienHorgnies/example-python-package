2974930696352acc3d1094b1ae2d0f09e37beb78
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-01-19 16:56:12 +0100

    implement prefix.timestamp

--- sql_check/prefix.py
+++ sql_check/prefix.py
@@ -1,11 +1,25 @@
-def timestamp(path, directory=None):
+import os.path
+from datetime import datetime
+from shutil import copyfile
+
+
+def timestamp(file_path, directory=None):
     """
     Create a copy of provided file prefixed with a timestamp
 
-    :param path: file to create a copy of
-    :type path: str
+    :param file_path: file to create a copy of
+    :type file_path: str
     :param directory: where to create the copy, default is next to the original file
     :type directory: str
     :return: absolute path to created copy
     """
-    return None
+    prefix = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+
+    copy_path = os.path.join(
+        directory if directory else os.path.dirname(file_path),
+        prefix + "_" + os.path.basename(file_path)
+    )
+
+    copyfile(file_path, copy_path)
+
+    return copy_path
