18d6fb806a5bf6a1417f51d567de785e957eb56a
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2019-01-19 10:00:08 +0100

    write tests

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sql_check',
+    version='1.0',
+    description='A module to run billing check, write reports and inspect results',
+    author='Adrien Horgnies',
+    author_email='aHorgnies@altissia.org',
+    packages=['sql_check'],
+    install_requires=[],
+)
--- tests/test_month_util.py
+++ tests/test_month_util.py
@@ -1,6 +1,15 @@
-def test_previous_month():
-    assert False
+from datetime import date
+
+import month_util
+
+
+def test_previous_month_nominal_case():
+    assert month_util.previous_month() == date(2016, 5, 1)
+
+
+def test_previous_month_january_case():
+    assert month_util.previous_month() == date(2016, 12, 1)
 
 
 def test_month_from_str():
-    assert False
+    assert month_util.month_from_str("201901") == date(2019, 1, 1)
