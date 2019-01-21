1954271c2ac58cb70a9ac03d5447cab299b8b277
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-21 16:21:44 +0100

    beautify message to ask password

--- sql_check/CursorProvider.py
+++ sql_check/CursorProvider.py
@@ -12,10 +12,13 @@ class CursorProvider(metaclass=Singleton):
         with open("application.yml", "r") as config_file:
             config = yaml.load(config_file)
 
+        host = config["mysql"]["host"]
+        user = config["mysql"]["user"]
+
         self.connection = connector.connect(
-            host=config["mysql"]["host"],
-            user=config["mysql"]["user"],
-            passwd=getpass.getpass()
+            host=host,
+            user=user,
+            passwd=getpass.getpass("Password for {user}@{host}: ".format(user=user, host=host))
         )
 
     def __del__(self):
