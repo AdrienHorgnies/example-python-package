8e2abedae696ed20391c0f701b57369f8ee65ebd
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-21 12:57:10 +0100

    define query.execute signature, specs and beginning of a test

--- sql_check/query.py
+++ sql_check/query.py
@@ -15,3 +15,15 @@ def str_from_file(file_path):
                       for line in open(file_path)
                       if not line.strip().startswith('--') and line.strip()
                   ]))
+
+
+def execute(file_path):
+    """
+    Execute the query, append a report to the file (start & end time, duration, n rows and result file) and copy results
+    next to the original file with the same file name but the extension being csv.
+
+    :param file_path: file containing a single SQL query
+    :type file_path: str
+    :return: the report of the query execution
+    """
+    return None
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,5 +1,11 @@
-import query
+import filecmp
+import os.path
 import tempfile
+from datetime import datetime
+
+import mock
+
+import query
 
 
 def test_str_from_file():
@@ -10,3 +16,33 @@ def test_str_from_file():
                    "WHERE 1;\n"
                    "\n")
     assert query.str_from_file(file.name) == "SELECT 'this is a test' WHERE 1;"
+
+
+@mock.patch("query.datetime")
+def test_execute(mock_datetime):
+    mock_datetime.now.side_effect = [
+        datetime(2019, 2, 3, 14, 52, 54, 452000),
+        datetime(2019, 2, 3, 14, 52, 55, 502000)
+    ]
+
+    # todo mock mysql connection provided
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as file:
+        file.write("SHOW DATABASES;\n")
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as expected_file:
+        expected_file.write("SHOW DATABASES;\n"
+                            "\n"
+                            "-- START TIME: 2019-02-03T14:52:54.452\n"
+                            "-- END TIME: 2019-02-03T14:52:55.502\n"
+                            "-- DURATION: 1.050s\n"
+                            "-- ROWS COUNT: 3\n"
+                            "-- RESULT FILE: {}\n".format(os.path.splitext(file.name)[0] + ".csv"))
+
+    results = query.execute(file.name)
+
+    assert filecmp.cmp(file, expected_file)
+    assert results == {
+        "n_rows": 3,
+        "headers": ['name'],
+        "rows": ['information_schema', 'database_a', 'database_b']
+    }
