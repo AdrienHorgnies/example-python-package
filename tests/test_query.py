83fe4b22829d9ffec82bcafc82b65ff15141b98e
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-21 09:55:44 +0100

    fix tests to avoid os errors

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,10 +3,10 @@ import tempfile
 
 
 def test_str_from_file():
-    with tempfile.TemporaryFile(mode='w') as file:
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as file:
         file.write("-- A comment followed by a blank line\n"
                    "\n"
                    "SELECT 'this is a test' -- inline comment\n"
                    "WHERE 1;\n"
                    "\n")
-        assert query.str_from_file(file.name) == "SELECT 'this is a test' WHERE 1;"
+    assert query.str_from_file(file.name) == "SELECT 'this is a test' WHERE 1;"
