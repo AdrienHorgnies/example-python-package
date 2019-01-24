cbced14bf307c8239754be3d462104bf5d30f27f
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-24 10:14:43 +0100

    test produced directory

--- tests/python/test_monthly_check.py
+++ tests/python/test_monthly_check.py
@@ -7,7 +7,7 @@ import mock
 from monthly_check import monthly_check
 
 
-@mock.patch("monthly_check.getpass.getuser")
+@mock.patch("monthly_check.getuser")
 @mock.patch("monthly_check.datetime")
 def test_monthly_check(mock_datetime, mock_getuser):
     mock_datetime.return_value = datetime(2019, 1, 23, 10, 31, 54)
@@ -20,6 +20,12 @@ def test_monthly_check(mock_datetime, mock_getuser):
     if not os.path.isdir(output):
         os.mkdir(output)
 
-    assert monthly_check(date(2019, 1, 1), source="test/assets", output=output)
+    produced_directory = monthly_check(date(2019, 1, 1), source="test/assets", output=output)
+
+    try:
+        assert produced_directory == os.path.join(output, "2019-01-January")
+        assert os.path.isdir(produced_directory)
+    finally:
+        os.removedirs(output)
 
     assert False
