874308eafae3fc221e6f0ecfa42c6722a7daa47f
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-25 12:14:09 +0100

    fix duration format for test resources

--- tests/resources/expected_monthly_check_report/2019-01-January/select-star/2019-01-23T10-31-54_select-star.sql
+++ tests/resources/expected_monthly_check_report/2019-01-January/select-star/2019-01-23T10-31-54_select-star.sql
@@ -5,6 +5,6 @@ FROM `some_table_that_I_dont_care_about_because_I_will_mock_database`;
 
 -- START TIME: 2019-01-23T10:31:54.450265
 -- END TIME: 2019-01-23T10:31:54.999999
--- DURATION: 0.549734
+-- DURATION: 00:00:00.549734
 -- ROWS COUNT: 0
 -- RESULT FILE: N/A
