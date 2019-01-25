5a9f360a29104561c2c70c6e3cd587c6f4917113
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-25 12:05:56 +0100

    mock database results

--- tests/python/test_monthly_check.py
+++ tests/python/test_monthly_check.py
@@ -1,3 +1,4 @@
+import csv
 import shutil
 import tempfile
 from datetime import date, datetime
@@ -29,10 +30,22 @@ def out_dir():
 
 @pytest.fixture(scope="module")
 def expected():
+    show_csv = join(X_REPORT_DIR, "show-columns", SHOW + ".csv")
+
+    with open(show_csv) as show_csv_file:
+        reader = csv.reader(show_csv_file)
+        csv_content = list(reader)
+        show_result = csv_content[1:]
+        show_description = csv_content[:1]
+
     return {
         "star_sql": join(X_REPORT_DIR, "select-star", STAR + ".sql"),
+        "star_result": [],
+        "star_description": [],
         "show_sql": join(X_REPORT_DIR, "show-columns", SHOW + ".sql"),
-        "show_csv": join(X_REPORT_DIR, "show-columns", SHOW + ".csv")
+        "show_csv": show_csv,
+        "show_result": show_result,
+        "show_description": show_description,
     }
 
 
@@ -42,6 +55,21 @@ def test_monthly_check(mock_cp, mock_prefix_dt, out_dir, expected):
     mock_prefix_dt.now.side_effect = [
         datetime(2019, 1, 23, 10, 31, 55),  # show-columns
         datetime(2019, 1, 23, 10, 31, 54),  # select-star
+    ]
+
+    instance = mock_cp.return_value
+
+    mock_cursor_show = mock.Mock()
+    mock_cursor_show.fetchall.return_value = expected["show_result"]
+    mock_cursor_show.description = expected["show_description"]
+
+    mock_cursor_star = mock.Mock()
+    mock_cursor_star.fetchall.return_value = expected["star_result"]
+    mock_cursor_star.description = expected["star_description"]
+
+    instance.cursor.side_effect = [
+        mock_cursor_show,
+        mock_cursor_star
     ]
 
     report_dir = monthly_check(date(2019, 1, 1), source="../resources/sql", output=out_dir)
