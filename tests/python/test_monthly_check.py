c0c2e07ca72e6a2f767736c992042480c1b4b625
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-25 09:40:59 +0100

    assert creation and content of queries and csv

--- tests/python/test_monthly_check.py
+++ tests/python/test_monthly_check.py
@@ -14,11 +14,29 @@ def test_monthly_check():
     os.mkdir(output)
 
     try:
-        produced_directory = monthly_check(date(2019, 1, 1), source="../resources/sql", output=output)
-        assert produced_directory == os.path.join(output, "2019-01-January")
-        assert os.path.isdir(produced_directory)
-        assert os.path.isdir(os.path.join(produced_directory, "select-star"))
-        assert os.path.isdir(os.path.join(produced_directory, "show-columns"))
-        assert os.path.isfile(os.path.join(produced_directory, "2019-01-January-report.md"))
+        report_dir = monthly_check(date(2019, 1, 1), source="../resources/sql", output=output)
+
+        assert report_dir == os.path.join(output, "2019-01-January")
+        assert os.path.isdir(report_dir)
+
+        assert os.path.isdir(os.path.join(report_dir, "select-star"))
+        assert os.path.isdir(os.path.join(report_dir, "show-columns"))
+        assert os.path.isfile(os.path.join(report_dir, "2019-01-January-report.md"))
+
+        expected_report_dir = "../resources/select-star/expected_monthly_check_report/2019-01-January"
+
+        star_query = os.path.join(report_dir, "select-star/2019-01-23T10-31-54_select-star.sql")
+        expected_star_query = os.path.join(expected_report_dir, "select-star/2019-01-23T10-31-54_select-star.sql")
+        assert os.path.isfile(star_query)
+        assert [row for row in open(star_query)] == [row for row in open(expected_star_query)]
+
+        show_query = os.path.join(report_dir, "show-columns/2019-01-23T10-31-55_show-columns.sql")
+        show_csv = os.path.join(report_dir, "show-columns/2019-01-23T10-31-55_show-columns.csv")
+        expected_show_query = os.path.join(expected_report_dir, "show-columns/2019-01-23T10-31-55_show-columns.sql")
+        expected_show_csv = os.path.join(expected_report_dir, "show-columns/2019-01-23T10-31-55_show-columns.csv")
+        assert os.path.isfile(show_query)
+        assert os.path.isfile(show_csv)
+        assert [row for row in open(show_query)] == [row for row in open(expected_show_query)]
+        assert [row for row in open(show_csv)] == [row for row in open(expected_show_csv)]
     finally:
         shutil.rmtree(output)
