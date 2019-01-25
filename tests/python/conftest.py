67d9ae2fc1200bab8a0c626dac78e57c79cceceb
Adrien Horgnies <aHorgnies@altissia.com>
2019-01-25 13:40:57 +0100

    less variables

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -5,15 +5,13 @@ import pytest
 
 RSRC = join("..", "resources")
 X_REPORT_DIR = join(RSRC, "expected_monthly_check", "2019-01-January")
-TMP_STAR = "2019-01-23T10-31-54_select-star"
-TMP_SHOW = "2019-01-23T10-31-55_show-columns"
 
 
 @pytest.fixture()
 def star():
     return {
         "in_sql": join(RSRC, "sql", "select-star.sql"),
-        "out_sql": join(X_REPORT_DIR, "select-star", TMP_STAR + ".sql"),
+        "out_sql": join(X_REPORT_DIR, "select-star", "2019-01-23T10-31-54_select-star.sql"),
         "rows": [],
         "description": []
     }
@@ -21,7 +19,7 @@ def star():
 
 @pytest.fixture()
 def show():
-    show_csv = join(X_REPORT_DIR, "show-columns", TMP_SHOW + ".csv")
+    show_csv = join(X_REPORT_DIR, "show-columns", "2019-01-23T10-31-55_show-columns.csv")
 
     with open(show_csv) as show_csv_file:
         reader = csv.reader(show_csv_file)
@@ -30,7 +28,7 @@ def show():
         show_description = csv_content[:1]
 
     return {
-        "show_sql": join(X_REPORT_DIR, "show-columns", TMP_SHOW + ".sql"),
+        "show_sql": join(X_REPORT_DIR, "show-columns", "2019-01-23T10-31-55_show-columns.sql"),
         "show_csv": show_csv,
         "show_result": show_result,
         "show_description": show_description,
